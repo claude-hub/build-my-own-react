adbb4ab40797541b70f920299205a5155088f3c9
claude-hub <314705487@qq.com>
2020-12-21 22:20:42 +0800

    add bindActionCreators

--- src/redux/mini-redux/index.js
+++ src/redux/mini-redux/index.js
@@ -1,5 +1,11 @@
 import createStore from './createStore';
 import applyMiddleware from './applyMiddleware';
 import combineReducers from './combineReducers';
+import bindActionCreators from './bindActionCreators';
 
-export { createStore, applyMiddleware, combineReducers };
+export {
+  createStore,
+  applyMiddleware,
+  combineReducers,
+  bindActionCreators
+};
