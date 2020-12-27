6f4a10a4653dfc9ed43452b8de7012d70ea4eb60
claude-hub <314705487@qq.com>
2020-12-27 21:20:19 +0800

    add react router dom demo

---
+++ src/react-router-dom/page/404.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class _404Page extends PureComponent {
+  render() {
+    return (
+      <div>404</div>
+    );
+  }
+}
+
+export default _404Page;
---
+++ src/react-router-dom/page/ReactRouterDomPage.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Test extends PureComponent {
+  render() {
+    return (
+      <div>123</div>
+    );
+  }
+}
+
+export default Test;
