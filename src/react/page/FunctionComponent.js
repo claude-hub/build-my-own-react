8a8b8b19ed230f27efbf2919f64acad8f0f4d6a8
claude-hub <314705487@qq.com>
2021-02-27 21:44:31 +0800

    update

--- src/react/page/FunctionComponent.js
+++ src/react/page/FunctionComponent.js
@@ -8,6 +8,13 @@ function funcComp(props) {
       <p>{`函数组件 - ${name}`}</p>
       <span>{count}</span>
       <button type="button" onClick={() => setCount(count + 1)}>Add</button>
+      <p>
+        <span>
+          {count % 2
+            ? <button type="button">click</button>
+            : <span>1231</span>}
+        </span>
+      </p>
     </div>
   );
 }
